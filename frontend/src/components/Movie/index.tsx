02df6d830cf95ba8b1784b81524b981db5ac8df9
Vsevolod Goloviznin <vsevolod.goloviznin@gmail.com>
2021-11-18 18:56:08 +0300

    - updating FE to account for new movie structure

--- frontend/src/components/Movie/index.tsx
+++ frontend/src/components/Movie/index.tsx
@@ -1,31 +1,33 @@
 import { useCallback } from 'react';
 import * as Styled from './components';
+import { MovieType } from '../../../../server/src/models/movie';
 
-export type Movie = {
-  id: number,
-  image_url: string,
-  title: string,
+function getImageUrl(item: MovieType): string {
+  const imageArray = item && item.images && (item.images.logos || item.images.backdrop);
+  if (imageArray) {
+    const images = imageArray.filter((f) => f.iso_639_1 === 'en').sort((a, b) => b.height - a.height);
+
+    if (images && images.length > 0) {
+      return `https://image.tmdb.org/t/p/original${images[0].file_path}`;
+    }
+  }
+
+  return '';
 }
 
 export default ({
-  id,
-  image_url,
-  title,
+  item,
   onClick
-}: Movie & {
-  onClick?: (item: Movie) => void
+}: {item: MovieType} & {
+  onClick?: (item: MovieType) => void
 }) => {
   const handleClick = useCallback(() => {
-    onClick && onClick({
-      id,
-      image_url,
-      title,
-    })
+    onClick && onClick(item)
   }, [])
   return (
     <Styled.Wrap onClick={handleClick} disabled={!onClick}>
-      <Styled.Container style={{ backgroundImage: `url(${image_url})` }}>
-
+      <Styled.Container style={{ backgroundImage: `url(${getImageUrl(item)})` }}>
+        {item.title}
       </Styled.Container>
     </Styled.Wrap>
   );
--- frontend/src/pages/Home/index.tsx
+++ frontend/src/pages/Home/index.tsx
@@ -1,19 +1,20 @@
 import { useCallback, useEffect, useState } from 'react';
 import api from '../../lib/api';
 import * as Styled from './components';
-import MovieItem, { Movie } from '../../components/Movie'
+import MovieItem from '../../components/Movie'
+import { MovieResponseType, MovieType } from '../../../../server/src/models/movie';
 
 export default () => {
-  const [items, setItems] = useState<Movie[]>([]);
-  const [cache, setCache] = useState<Movie[]>([]);
+  const [response, setItems] = useState<MovieResponseType>();
+  const [cache, setCache] = useState<MovieType[]>([]);
 
   useEffect(() => {
-    api.get<Movie[]>('/movies').then((response) => setItems(response.data));
+    api.get<MovieResponseType>('/movies').then((response) => setItems(response.data));
   }, []);
 
-  const onClickItem = useCallback((item) => {
+  const onClickItem = useCallback((item: MovieType) => {
     setCache(c => [...c, item])
-    api.get<Movie[]>(`/movies/${item.id}/similar`).then((response) => setItems(response.data));
+    api.get<MovieResponseType>(`/movies`).then((response) => setItems(response.data));
   }, [])
 
   return (
@@ -27,24 +28,29 @@ export default () => {
           Some words about demo and how it could be used are written here... <br /> two lines will be good!
         </Styled.Description>
       </Styled.Container>
+      <Styled.Container>
+        <Styled.Description>
+          Randomly selected category <b>{response?.tag}</b>
+        </Styled.Description>
+      </Styled.Container>
       <Styled.ItemsList>
         {
-          cache.map((item) =>
-          <MovieItem
-            key={item.id}
-            {...item}
-          />
+          cache?.map((item) =>
+            <MovieItem
+              key={item.id}
+              item={item}
+            />
           )
         }
       </Styled.ItemsList>
       <hr />
       <Styled.ItemsList>
         {
-          items.map((item) =>
+          response?.movies.map((item) =>
             <MovieItem
               key={item.id}
               onClick={onClickItem}
-              {...item}
+              item={item}
             />
           )
         }
