54419b6cc3c463a22da20630c826fc94226f7fce
Bitaru <i@bitaru.com>
2022-01-21 15:44:51 +0300

    fix env variable

--- frontend/src/lib/api.ts
+++ frontend/src/lib/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const root = axios.create({
-  baseURL: process.env.api_root || `http://localhost:${process.env.PORT || 3001}`,
+  baseURL: process.env.API_ROOT || `http://localhost:${process.env.PORT || 3001}`,
 });
 
 export default root;
